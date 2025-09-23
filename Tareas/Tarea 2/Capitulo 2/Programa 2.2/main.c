c0e4c91d7786224fc9e077b5321576db308cde78
Julio Valdes <julioedo19@gmail.com>
2025-09-23 01:53:44 -0400

    Programa 2.2 terminado

--- Tareas/Tarea 2/Capitulo 2/Programa 2.2/main.c
+++ Tareas/Tarea 2/Capitulo 2/Programa 2.2/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+/* Incremento de precio.
+El prorama, al recibir como dato el precio de un producto importado,
+incrementa 11% el mismo si este es inferior a $1,500.
+PRE y NPR: variable de tipo real. */
 
-int main()
+void main (void)
 {
-    printf("Hello world!\n");
-    return 0;
+    float PRE, NPR;
+    printf("Ingrese el precio del producto: ");
+    scanf("%f", &PRE);
+    if (PRE > 1500)
+    {
+        NPR = PRE * 1.11;
+        printf("\nNuevo Precio: %7.2f", NPR);
+    }
+
 }
