1bac738c1cf481048317154192d648e228519f6c
Julio Valdes <julioedo19@gmail.com>
2025-09-25 23:28:43 -0400

    Terminado

---
+++ Tareas/Tarea 2/Capitulo 3/Programa 3.4/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Suma cuadrado
+El programa, al recibir com odatos un grupo de enteros positivos, obtiene el
+cuadrado de los mismos y la suma correspondientes a dichos cuadrados. */
+
+void main(void)
+{
+    int NUM;
+    long CUA, SUC = 0;
+    printf("\nIngrese un numero -0 para terminar-:\t");
+    scanf("%d", &NUM);
+    while (NUM)
+    {
+        CUA = pow (NUM,2);
+        printf("%d al cubo es %ld", NUM, CUA);
+        SUC = SUC + CUA;
+        printf("\nIngrese un numero entero -0 para terminar-:\t");
+        scanf("%d", &NUM);
+    }
+    printf("\nLa suma de los cuadrados de %d", SUC);
+}
