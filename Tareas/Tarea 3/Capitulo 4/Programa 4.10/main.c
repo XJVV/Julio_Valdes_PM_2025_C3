087da4658ec44df50949ad49ba4ca7e55dfa9103
Julio Valdes <julioedo19@gmail.com>
2025-10-02 19:14:44 -0400

    Listo

---
+++ Tareas/Tarea 3/Capitulo 4/Programa 4.10/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*Multiplo
+el programa, al recibir como datos dos numeros enteros, determina si
+el segundo es multiplo del primero. */
+
+int multiplo(int, int);     /*Prototipo de funcion*/
+
+void main(void)
+{
+    int NU1, NU2, RES;
+    printf("\nIngrese los dos numeros: ");
+    scanf("%d %d", &NU1, &NU2);
+    RES = multiplo(NU1, NU2);
+    if (RES)
+        printf("\nEl segungo numero es multiplo del primer numero");
+    else
+        printf("\nEl segundo numero no es multiplo del primero");
+}
+
+int multiplo(int N1, int N2)
+/* Esta funcion determina si N2 es multiplo de N1. */
+{
+    int RES;
+    if ((N2 % N1) == 0)
+        RES = 1;
+    else
+        RES = 0;
+    return(RES);
+}
